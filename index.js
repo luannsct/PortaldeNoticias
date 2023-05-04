2b473da80e40a595763c20b405c7b8218eb8e472
luannsct <luan.nsct@gmail.com>
2023-05-03 22:35:03 -0300

    Criando um portal de noticias estatico

---
+++ index.js
@@ -0,0 +1,29 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const  path  = require('path');
+
+const app = express();
+
+app.use( bodyParser.json()); //Para suportar JSON ENCODED 
+app.use(bodyParser.urlencoded({ //PARA SUPORTAR URLENCODE 
+    extended:true
+}));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
+app.use('/public', express.static(path.join(__dirname,'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+//Home
+app.get('/',(req,res)=>{
+    res.send('Home!');
+})
+//Recupera a rota! ex. site/slug
+app.get('/:slug',(req,res)=>{
+    res.send(req.params.slug);
+})
+
+
+app.listen(5000,()=>{
+    console.log('Servidor Online');
+})
